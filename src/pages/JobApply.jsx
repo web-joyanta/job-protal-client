d71a0126dd1fd0df281a5cf11b6b14e873dd7789
web-joyanta <web.joyanta@gmail.com>
2025-08-13 00:06:33 +0600

    fetch to post method job application data and sweet alert data added

--- src/pages/JobApply.jsx
+++ src/pages/JobApply.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
     const { id } = useParams();
@@ -19,7 +20,27 @@ const JobApply = () => {
             github,
             resume,
         }
-        console.log(jobApplications)
+
+        fetch(("http://localhost:5000/job-applications"), {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(jobApplications)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    form.reset();
+                }
+            })
 
     }
     return (
