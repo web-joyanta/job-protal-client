dbb7c35302e405b1e3ecef345027d21add3def4c
web-joyanta <web.joyanta@gmail.com>
2025-08-20 15:16:38 +0600

    my posted jobs table data showing and job application count

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -6,13 +6,38 @@ const MyPostedJob = () => {
     const { user } = useAuth();
     useEffect(() => {
         fetch(`http://localhost:5000/jobs?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setJobs(data))
+            .then(res => res.json())
+            .then(data => setJobs(data))
     }, [user.email]);
 
     return (
         <div>
-            <h1>My Posted Job {jobs.length}</h1>
+            <h1 className="text-2xl font-medium mb-2">My Posted Job {jobs.length}</h1>
+            <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Title</th>
+                            <th>Deadline</th>
+                            <th>Application Count</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <tr key={job._id}>
+                                <th>{index + 1}</th>
+                                <td>{job.title}</td>
+                                <td>{job.deadline}</td>
+                                <td>{job.applicationCount}</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
