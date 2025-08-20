e338bd071e64202569196ce6889038349e28b342
web-joyanta <web.joyanta@gmail.com>
2025-08-20 19:13:21 +0600

    status change or unick id for fetch status change or sweet alert

--- src/pages/ViewApplications.jsx
+++ src/pages/ViewApplications.jsx
@@ -1,7 +1,34 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
     const applications = useLoaderData();
+    const handleStatusChange = (e, id) => {
+        const data = {
+            status: e.target.value
+        };
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Status has been updated",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+        // Here you can add the logic to update the status in the backend
+        // For example, you might want to send a PUT request to update the application status
+    }
     return (
         <div>
             <h2 className="text-2xl font-medium mb-2">View Applications: {applications.length}</h2>
@@ -13,17 +40,25 @@ const ViewApplications = () => {
                             <th></th>
                             <th>Email</th>
                             <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Update Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             applications.map((app, index) => <tr key={app._id}>
-                            <th>{index + 1}</th>
-                            <td>{app.applicant_email}</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>)
+                                <th>{index + 1}</th>
+                                <td>{app.applicant_email}</td>
+                                <td>Quality Control Specialist</td>
+                                <td>
+                                    <select onChange={(e) => handleStatusChange(e, app._id)} defaultValue={app.status || 'Change Status'} className="select select-bordered  select-xs w-full max-w-xs">
+                                        <option disabled={true}>Change Status</option>
+                                        <option>Hired</option>
+                                        <option>Set Interview</option>
+                                        <option>Under Review</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>)
                         }
                     </tbody>
                 </table>
